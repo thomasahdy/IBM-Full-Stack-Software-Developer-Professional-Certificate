5516214d13c3ca4cdd1289c74cc536540f1bc426
thomasahdy <thomasahdy274@gmail.com>
2025-07-14 23:11:47 -0400

    todo add clrea all btn

--- JavaScript_Programming_Essentials/TodoList/todo_list.js
+++ JavaScript_Programming_Essentials/TodoList/todo_list.js
@@ -2,6 +2,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearComletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllBtn = document.getElementById("clearAllBtn");
 
 let tasks = [];
 
@@ -36,7 +37,11 @@ function clearCompletedTasks() {
     tasks = tasks.filter(task => !task.completed);
     displayTasks();
 }
-
+function clearAllTasks () {
+    tasks = [];
+    displayTasks();
+}
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAllTasks);
 displayTasks();
